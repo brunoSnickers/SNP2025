7dd298d2167a9dd656ef42d8c6dfdf93a7c4abe5
brunoSnickers <152272959+brunoSnickers@users.noreply.github.com>
2025-03-23 12:59:28 +0100

    Uhrenquartz ist zu kalt. allOn.c
    error volotile double
    check error funkt
    seconds = seconds - 60; damit falls eine schalteskeunde auf das ende trifft wir diese sekunde nicht verschlucken.

--- allOn.c
+++ allOn.c
@@ -7,6 +7,7 @@
 volatile uint8_t seconds = 0;
 volatile uint8_t minutes = 0;
 volatile uint8_t hours = 0;
+volatile double error = 0.0;
 
 // Helligkeitsstufen für PWM
 int currentBrightnessLevel = 1;
@@ -106,11 +107,21 @@ void displayTime(void) {
     PORTD = (PORTD & 0x07) | ((hours & 0x1F) << 3);
 }
 
+// Korrigert für nicht optimale Temperatur des Uhrenquartz
+void check_error(void){
+    error = error + 0.01875;  // Fehlende Sekunden pro gezählte Sekunde
+    if (error >= 1.0) { // Wenn sich eine Schaltsekunde Summiert hat:
+        seconds++;
+        error = error - 1.0;
+    }
+}
+
 // Uhrzeit aktualisieren
 void updateClock(void) {
     seconds++;
+    check_error();
     if (seconds >= 60) {
-        seconds = 0;
+        seconds = seconds - 60;
         minutes++;
         if (minutes >= 60) {
             minutes = 0;
